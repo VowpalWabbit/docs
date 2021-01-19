c434f53b8b0519a93cff8c60acdd9166c7d5cafc
WoboWabbit <WoboWabbit@hunch.net>
2021-01-19 22:07:15 +0000

    Update documentation for commit: VowpalWabbit/vowpal_wabbit@145684f6205c7fa8371b89719c63574f8934d856

--- vowpal_wabbit/cpp/latest/search/all_8.js
+++ vowpal_wabbit/cpp/latest/search/all_8.js
@@ -20,7 +20,7 @@ var searchData=
   ['hashstring',['hashstring',['../hashstring_8h.html#ad0c610591e5f64e2d6e5753b6f4266f8',1,'hashstring.h']]],
   ['hashstring_2eh',['hashstring.h',['../hashstring_8h.html',1,'']]],
   ['head',['head',['../structVW_1_1config_1_1typelist.html#a53badf648cb2451504736dc2a9e02d3e',1,'VW::config::typelist']]],
-  ['help',['help',['../structVW_1_1config_1_1option__builder.html#af0ab17d905621f08b1545d575c132dab',1,'VW::config::option_builder::help()'],['../structVW_1_1config_1_1options__i.html#a068859ff0a9f9fc8934e3f3215e62926',1,'VW::config::options_i::help()'],['../structVW_1_1config_1_1options__name__extractor.html#aeeb8db957c5cdf8835a1d0a5f72746af',1,'VW::config::options_name_extractor::help()'],['../structVW_1_1config_1_1options__boost__po.html#a4718e4d9e1ad9bd919f1c1b1950cc252',1,'VW::config::options_boost_po::help()']]],
+  ['help',['help',['../structVW_1_1config_1_1option__builder.html#af0ab17d905621f08b1545d575c132dab',1,'VW::config::option_builder::help()'],['../structVW_1_1config_1_1options__i.html#a126d126a40231ce81a2a637a39839897',1,'VW::config::options_i::help()'],['../structVW_1_1config_1_1options__name__extractor.html#accb9377125747bd5b67db30741584e8a',1,'VW::config::options_name_extractor::help()'],['../structVW_1_1config_1_1options__boost__po.html#a1569ac958b3e8de5c61ea7c154883401',1,'VW::config::options_boost_po::help()']]],
   ['hessian_5fon',['hessian_on',['../structvw.html#aba81c9a5f90eda032abe3f2977a77698',1,'vw']]],
   ['history_5fnamespace',['history_namespace',['../constant_8h.html#a622e9192f051b3662837af6d3d0fbac8',1,'constant.h']]],
   ['holdout_5fafter',['holdout_after',['../structvw.html#a1caf02baa08f9e1e156615b98bbbd196',1,'vw']]],
