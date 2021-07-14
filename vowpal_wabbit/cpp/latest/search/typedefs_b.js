5ba8ba0be4145c5f860075f5018df13db29244e2
WoboWabbit <WoboWabbit@hunch.net>
2021-07-14 10:33:39 +0000

    Update documentation for commit: VowpalWabbit/vowpal_wabbit@96ed8316de4391b77f4f29af69f885552a644769

--- vowpal_wabbit/cpp/latest/search/typedefs_b.js
+++ vowpal_wabbit/cpp/latest/search/typedefs_b.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['namespace_5findex',['namespace_index',['../example__predict_8h.html#a3302327c8e16f985dd6c4474bf279c40',1,'namespace_index():&#160;example_predict.h'],['../namespaced__features_8h.html#a3302327c8e16f985dd6c4474bf279c40',1,'namespace_index():&#160;namespaced_features.h']]],
+  ['namespace_5findex',['namespace_index',['../example__predict_8h.html#a3302327c8e16f985dd6c4474bf279c40',1,'namespace_index():&#160;example_predict.h'],['../namespaced__feature__store_8h.html#a3302327c8e16f985dd6c4474bf279c40',1,'namespace_index():&#160;namespaced_feature_store.h']]],
   ['no_5flabel',['no_label',['../namespaceno__label.html#a85a10f8b84a537aa9f3da50a4e566a4c',1,'no_label']]]
 ];
