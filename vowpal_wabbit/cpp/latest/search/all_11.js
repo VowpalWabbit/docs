aa709c009941cd18a65b9e02120d4e077600e678
WoboWabbit <WoboWabbit@hunch.net>
2022-06-17 19:11:47 +0000

    Update documentation for commit: VowpalWabbit/vowpal_wabbit@8a016e1b66f53e6f016ae195a092e6baeccec2a3

--- vowpal_wabbit/cpp/latest/search/all_11.js
+++ vowpal_wabbit/cpp/latest/search/all_11.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['qfunc',['qfunc',['../structVW_1_1distributionally__robust_1_1Duals.html#a9627e9fe4e349480992e52f79006daf9',1,'VW::distributionally_robust::Duals']]],
-  ['qlb',['qlb',['../classVW_1_1distributionally__robust_1_1ChiSquared.html#abb2067abd2a6e200dffecb40c5dc31aa',1,'VW::distributionally_robust::ChiSquared']]],
+  ['qfunc',['qfunc',['../structVW_1_1distributionally__robust_1_1Duals.html#aa6aab306e6dbac314ee6ce849a1570fb',1,'VW::distributionally_robust::Duals']]],
+  ['qlb',['qlb',['../classVW_1_1distributionally__robust_1_1ChiSquared.html#ae78f8bffb99b858cbcb5be5a550a5a17',1,'VW::distributionally_robust::ChiSquared']]],
   ['qr_5fdecomposition_2eh',['qr_decomposition.h',['../qr__decomposition_8h.html',1,'']]],
   ['quadratic_5fconstant',['quadratic_constant',['../constant_8h.html#a758aed5483f99f3e678868cd399f0efa',1,'constant.h']]],
   ['queries',['queries',['../structshared__data.html#a4727f86fcc45a0a9bc961efd1ea13836',1,'shared_data']]],
