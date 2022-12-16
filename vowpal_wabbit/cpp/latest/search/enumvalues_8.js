99afc91cfaa391abb0b312e32c5d80aed21c887b
WoboWabbit <WoboWabbit@hunch.net>
2022-12-16 22:50:20 +0000

    Update documentation for commit: VowpalWabbit/vowpal_wabbit@2bcac0c6e34ca87f8bb8539fda4e62b46a26bea6

--- vowpal_wabbit/cpp/latest/search/enumvalues_8.js
+++ vowpal_wabbit/cpp/latest/search/enumvalues_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['off_5flevel_4585',['OFF_LEVEL',['../namespaceVW_1_1io.html#aaf6d3dfe9781b53c9fbc2908a8f0641cad47e3bea3f69cebbbb7435e30735edd2',1,'VW::io']]]
+  ['off_5flevel_4584',['OFF_LEVEL',['../namespaceVW_1_1io.html#aaf6d3dfe9781b53c9fbc2908a8f0641cad47e3bea3f69cebbbb7435e30735edd2',1,'VW::io']]]
 ];
--- vowpal_wabbit/cpp/latest/search/related_5.js
+++ vowpal_wabbit/cpp/latest/search/related_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['swap_4628',['swap',['../classVW_1_1details_1_1audit__features__iterator.html#a502f653e52f0e7834af4ab51537dc02f',1,'VW::details::audit_features_iterator::swap()'],['../classVW_1_1details_1_1features__iterator.html#a566ea0516d309f8c174d2e1a04dda00d',1,'VW::details::features_iterator::swap()']]]
+  ['swap_4627',['swap',['../classVW_1_1details_1_1audit__features__iterator.html#a502f653e52f0e7834af4ab51537dc02f',1,'VW::details::audit_features_iterator::swap()'],['../classVW_1_1details_1_1features__iterator.html#a566ea0516d309f8c174d2e1a04dda00d',1,'VW::details::features_iterator::swap()']]]
 ];
