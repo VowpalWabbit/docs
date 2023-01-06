2bcde88adf3a6494620d48dc0530f1f4741e24b6
WoboWabbit <WoboWabbit@hunch.net>
2023-01-06 23:16:27 +0000

    Update documentation for commit: VowpalWabbit/vowpal_wabbit@fc9ab254907b32ff2103d5ad64745f2bb2a76216

--- vowpal_wabbit/cpp/latest/search/enumvalues_1.js
+++ vowpal_wabbit/cpp/latest/search/enumvalues_1.js
@@ -10,6 +10,6 @@ var searchData=
   ['continuous_7',['CONTINUOUS',['../namespaceVW.html#a41c81f994e2400fabf138c9f2b3d78e5ab1fa9dd3af034b3ef4291579aa673c07',1,'VW']]],
   ['continuous_8',['continuous',['../namespaceVW.html#a41c81f994e2400fabf138c9f2b3d78e5ad2884efee751eb4746b685827c7efcd9',1,'VW']]],
   ['critical_5flevel_9',['CRITICAL_LEVEL',['../namespaceVW_1_1io.html#aaf6d3dfe9781b53c9fbc2908a8f0641ca40c18d704f38d99ee7221f2ddbaf76fa',1,'VW::io']]],
-  ['cs_10',['CS',['../namespaceVW.html#a41c81f994e2400fabf138c9f2b3d78e5a8d7e99c73cd5a10adaaf4c9f9a520368',1,'VW']]],
-  ['cs_11',['cs',['../namespaceVW.html#a41c81f994e2400fabf138c9f2b3d78e5a95cc64dd2825f9df13ec4ad683ecf339',1,'VW']]]
+  ['cs_10',['cs',['../namespaceVW.html#a41c81f994e2400fabf138c9f2b3d78e5a95cc64dd2825f9df13ec4ad683ecf339',1,'VW']]],
+  ['cs_11',['CS',['../namespaceVW.html#a41c81f994e2400fabf138c9f2b3d78e5a8d7e99c73cd5a10adaaf4c9f9a520368',1,'VW']]]
 ];
