9d7698b57e06eb17b19a01399fa179fdb3e2416c
WoboWabbit <WoboWabbit@hunch.net>
2023-02-22 15:09:38 +0000

    Update documentation for commit: VowpalWabbit/vowpal_wabbit@b89b4242de64efb0df0cb6601a7126e893001f96

--- vowpal_wabbit/cpp/latest/search/enumvalues_0.js
+++ vowpal_wabbit/cpp/latest/search/enumvalues_0.js
@@ -5,8 +5,8 @@ var searchData=
   ['action_5fpdf_5fvalue_2',['action_pdf_value',['../namespaceVW.html#a4f76045a06b3efe3c5c612b777b6c836a4a0b76be67ec342548cde44e9d897849',1,'VW']]],
   ['action_5fprobs_3',['ACTION_PROBS',['../namespaceVW.html#a4f76045a06b3efe3c5c612b777b6c836a029d9bb33faffbec445f7f24a65461c9',1,'VW']]],
   ['action_5fprobs_4',['action_probs',['../namespaceVW.html#a4f76045a06b3efe3c5c612b777b6c836aa78dde72b6338b01742027024d34eca6',1,'VW']]],
-  ['action_5fscores_5',['ACTION_SCORES',['../namespaceVW.html#a4f76045a06b3efe3c5c612b777b6c836a5f4f7cf4a4acb8b44fc3284a75308a57',1,'VW']]],
-  ['action_5fscores_6',['action_scores',['../namespaceVW.html#a4f76045a06b3efe3c5c612b777b6c836a43568e6f9bdfff4805f9154606cde508',1,'VW']]],
-  ['active_5fmulticlass_7',['ACTIVE_MULTICLASS',['../namespaceVW.html#a4f76045a06b3efe3c5c612b777b6c836a53166887a33a44e4dbf67777ce71ae5e',1,'VW']]],
-  ['active_5fmulticlass_8',['active_multiclass',['../namespaceVW.html#a4f76045a06b3efe3c5c612b777b6c836a737dd71e6fe9512b7b999ac567613bb8',1,'VW']]]
+  ['action_5fscores_5',['action_scores',['../namespaceVW.html#a4f76045a06b3efe3c5c612b777b6c836a43568e6f9bdfff4805f9154606cde508',1,'VW']]],
+  ['action_5fscores_6',['ACTION_SCORES',['../namespaceVW.html#a4f76045a06b3efe3c5c612b777b6c836a5f4f7cf4a4acb8b44fc3284a75308a57',1,'VW']]],
+  ['active_5fmulticlass_7',['active_multiclass',['../namespaceVW.html#a4f76045a06b3efe3c5c612b777b6c836a737dd71e6fe9512b7b999ac567613bb8',1,'VW']]],
+  ['active_5fmulticlass_8',['ACTIVE_MULTICLASS',['../namespaceVW.html#a4f76045a06b3efe3c5c612b777b6c836a53166887a33a44e4dbf67777ce71ae5e',1,'VW']]]
 ];
