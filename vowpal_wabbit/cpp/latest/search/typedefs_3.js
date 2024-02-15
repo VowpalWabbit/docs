b3d95d0dc3fd58af9e43f1e0770b09002788622b
WoboWabbit <WoboWabbit@hunch.net>
2024-02-15 19:51:01 +0000

    Update documentation for commit: VowpalWabbit/vowpal_wabbit@305a795d5fe30c95ed308b3846fe4aacfc1c9a59

--- vowpal_wabbit/cpp/latest/search/typedefs_3.js
+++ vowpal_wabbit/cpp/latest/search/typedefs_3.js
@@ -6,7 +6,8 @@ var searchData=
   ['example_5ffactory_5ft_3',['example_factory_t',['../namespaceVW.html#a5489a0fd240d8221e205b8773406f819',1,'VW']]],
   ['example_5ffunc_4',['example_func',['../namespaceVW_1_1LEARNER_1_1details.html#a4da1adeb0dd812ef4f71fe8abf78deb4',1,'VW::LEARNER::details']]],
   ['example_5fpredict_5',['example_predict',['../example__predict_8h.html#ae508cf02fc304e96bda9fbf66296d4f7',1,'example_predict.h']]],
-  ['example_5ftype_6',['example_type',['../namespaceCCB.html#a64fe3c3075c4064b91150d0d7e4a11e9',1,'CCB']]],
-  ['extent_5fiterator_7',['extent_iterator',['../classVW_1_1features.html#ac00a3b2f0e39487cc65a98ed12828915',1,'VW::features']]],
-  ['extent_5fterm_8',['extent_term',['../namespaceVW.html#aae9a8744cdc9c5ec6f9745986feeff35',1,'VW::extent_term()'],['../feature__group_8h.html#a8bb81b5203696361337bfa09d86eb41c',1,'extent_term():&#160;feature_group.h']]]
+  ['example_5fsink_5ff_6',['example_sink_f',['../namespaceVW.html#aff3702b28a27a39b71e86b253e59fc26',1,'VW']]],
+  ['example_5ftype_7',['example_type',['../namespaceCCB.html#a64fe3c3075c4064b91150d0d7e4a11e9',1,'CCB']]],
+  ['extent_5fiterator_8',['extent_iterator',['../classVW_1_1features.html#ac00a3b2f0e39487cc65a98ed12828915',1,'VW::features']]],
+  ['extent_5fterm_9',['extent_term',['../namespaceVW.html#aae9a8744cdc9c5ec6f9745986feeff35',1,'VW::extent_term()'],['../feature__group_8h.html#a8bb81b5203696361337bfa09d86eb41c',1,'extent_term():&#160;feature_group.h']]]
 ];
