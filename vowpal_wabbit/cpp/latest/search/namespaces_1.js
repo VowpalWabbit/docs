4f3c0db1b1597a99babab689a4035d9c4a3c56fd
WoboWabbit <WoboWabbit@hunch.net>
2026-02-18 01:30:11 +0000

    Update documentation for commit: VowpalWabbit/vowpal_wabbit@c26ac262f2f6b35607e0571d1764d4a4ad817819

--- vowpal_wabbit/cpp/latest/search/namespaces_1.js
+++ vowpal_wabbit/cpp/latest/search/namespaces_1.js
@@ -2,8 +2,7 @@ var searchData=
 [
   ['cb_0',['CB',['../namespaceCB.html',1,'']]],
   ['cb_5fadf_1',['CB_ADF',['../namespaceCB__ADF.html',1,'']]],
-  ['cb_5falgs_2',['CB_ALGS',['../namespaceCB__ALGS.html',1,'']]],
-  ['cb_5feval_3',['CB_EVAL',['../namespaceCB__EVAL.html',1,'']]],
-  ['ccb_4',['CCB',['../namespaceCCB.html',1,'']]],
-  ['cost_5fsensitive_5',['COST_SENSITIVE',['../namespaceCOST__SENSITIVE.html',1,'']]]
+  ['cb_5feval_2',['CB_EVAL',['../namespaceCB__EVAL.html',1,'']]],
+  ['ccb_3',['CCB',['../namespaceCCB.html',1,'']]],
+  ['cost_5fsensitive_4',['COST_SENSITIVE',['../namespaceCOST__SENSITIVE.html',1,'']]]
 ];
